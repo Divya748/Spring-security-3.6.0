db588e77a49df9b1154b43963e40648046aa3c74
divyadivya8 <divyadivya8@host.docker.internal>
2023-04-25 17:36:36 +0530

    spring-security(4/25/2023)

---
+++ src/main/java/com/divya/security/controller/CsrfController.java
@@ -0,0 +1,17 @@
+package com.divya.security.controller;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+public class CsrfController {
+
+	@GetMapping("/csrf-token")
+	public CsrfToken retrieveCsrfToken(HttpServletRequest request) {
+		return (CsrfToken) request.getAttribute("_csrf");
+	}
+
+}
